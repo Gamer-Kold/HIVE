ab6d6c3502ced4ffc00b7251cdd6671cd969750e
Talha Qamar <qamartalha@proton.me>
2023-05-09 07:22:46 +0500

    Set fixed size for level

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int levelSideLength = 25;
+const int tilesSizeInPixels = 40;
+
+
 enum Tiles{
 	NOTHING,
 	WALL,
@@ -10,60 +14,55 @@ enum Tiles{
 	WIN
 };
 
-struct Level{
-	int width;
-	int height;
-	enum Tiles* tiles;
-};
 
 
-int mod(int a, int b)
-{
+int mod(int a, int b){
 	int div = a / b;
 
 	return (a - (div * b));
 	
 }
 
-void DrawLevel(struct Level lvl){
-	for(size_t i = 0; i < 100 ; i++)
-	{
-		Color col;
-		if(lvl.tiles[i] == WALL)
-			col = GRAY;
-		else if (lvl.tiles[i] == PLAYER)
-			col = BLUE;
-		else if (lvl.tiles[i] == WIN)
-			col = GREEN;
-		else 
-			continue;
-		int x = mod(i, 10);
-		int y = i/10;
 
-		DrawRectangle(x * 100,  y * 100, 100, 100, col);
+void DrawLevel(enum Tiles* lvl){
+	
+	const Color col[] = {GRAY, BLUE, GREEN};
+	for (size_t i = 0; i < (levelSideLength * levelSideLength); i++) {
+		if(lvl[i] == NOTHING)
+			continue;
+
+		int x = mod(i, levelSideLength);
+		int y = i / levelSideLength;
+
+		DrawRectangle(x * tilesSizeInPixels, y * tilesSizeInPixels, tilesSizeInPixels, tilesSizeInPixels, col[lvl[i] - 1]);
 	}
 }
+
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main(void)
-{
+int main(void){
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 1000;
     const int screenHeight = 1000;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    struct Level test = {10, 10, malloc(sizeof(enum Tiles) * 10 * 10)};
+    enum Tiles level[levelSideLength * levelSideLength];
 
-    for(size_t i = 0; i < 100 ; i++)
-    {
-	test.tiles[i] = NOTHING;
+
+    for (size_t i = 0; i < (levelSideLength * levelSideLength); i++) {
+	    level[i] = NOTHING;
     }
 
-    test.tiles[5] = PLAYER;
+    level[6] = WALL;
+    level[7] = PLAYER;
+    level[8] = WIN;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -82,17 +81,7 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-	    DrawLevel(test);
-
-	    if(IsMouseButtonDown(MOUSE_LEFT_BUTTON))
-	    {
-		    int x = GetMouseX() / 100;
-		    int y = GetMouseY() / 100;    
-
-		    test.tiles[(y * 10) + x] = WALL;
-	    }
+	    DrawLevel(level);
 
 	    if(IsKeyDown(KEY_W))
 			    printf("W");
@@ -112,8 +101,5 @@ int main(void)
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
-
-    free(test.tiles);
-
     return 0;
 }
