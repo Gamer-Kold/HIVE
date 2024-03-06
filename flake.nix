bd816f2aa3987d5c37580df11e0439427fb8b6ff
Talha Qamar <qamartalha@proton.me>
2024-03-06 20:48:12 +0500

    Hopefully zig 0.10 works

--- flake.nix
+++ flake.nix
@@ -4,17 +4,16 @@
   inputs = 
   { 
   nixpkgs.url = "github:nixos/nixpkgs/23.11"; 
-  zig.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, zig, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
     in {
       devShells.x86_64-linux.default = pkgs.mkShell {
         name = "HIVE build environment";
         buildInputs = [
-	zig.packages.master
+        pkgs.zig_0_10
 	pkgs.clang
         ];
       };
