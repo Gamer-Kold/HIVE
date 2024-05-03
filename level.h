c3bd8989200e24ed79f73bed0ee7b1d060d0e560
Talha Qamar <qamartalha@proton.me>
2024-05-03 20:57:07 +0500

    WIP new level format, changed type specs

--- level.h
+++ level.h
@@ -1,14 +1,17 @@
 
+#include <stddef.h>
 #include <stdint.h>
 typedef enum{
-	EMPTY,
-	BEE,
-	FLOWER,
+	NONE,
 	WALL,
 } Tiles;
 
 typedef struct{
 	Tiles* level;
+	int* bees;
+	size_t number_of_bees;
+	int* flowers;
+	size_t number_of_flowers;
 	uint8_t width;
 	uint8_t height;
 } Level;
--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include "level.h"
 
 
+typedef struct{} GameState;
 
 const Color level_colors[] = {BLACK, YELLOW, PINK, WHITE};
 void DrawLevel(Level level){
 	for(int i = 0; i < level.width; i++){
 		for(int j = 0; j < level.height; j++){
-			Tiles tile = level.level[(i * level.width) + j];
-			DrawRectangle(i * 100, j * 100, 100, 100, level_colors[tile]);
+		Tiles tile = level.level[(j * level.height) + i];
+			DrawRectangle(i * 50 + 5, j * 50 + 5, 50 - 10, 50 - 10, level_colors[tile]);
 		}
 	}
 }
@@ -16,10 +17,13 @@ void DrawLevel(Level level){
 
 int main(){
 	InitWindow(500, 500, "HIVE");
+	GameState game_state = {0};
 	while(!WindowShouldClose()){
 		BeginDrawing();
-		ClearBackground(RED);
+		ClearBackground(BLACK);
+		game_state = UpdateGameState(game_state);
 		DrawLevel(GetLevel(0));
+		DrawGameState(game_state);
 		EndDrawing();
 	}
 
