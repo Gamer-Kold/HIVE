016cd615ccbf43352bfde58fd1d7e8ebf167e11c
Talha Qamar <qamartalha@proton.me>
2024-05-14 18:06:38 +0500

    Removed plug_main.h

--- plug_main.h
+++
@@ -1,9 +0,0 @@
-#include "level.h"
-#ifndef PLUG_MAIN_H_INCLUDED
-#define PLUG_MAIN_H_INCLUDED
-typedef struct{
-	Level current_level;
-	char* current_level_file;
-	int something;
-} GameState;
-#endif
